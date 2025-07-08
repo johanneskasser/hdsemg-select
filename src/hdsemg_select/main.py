134fad3a8bb33f1a9f513e83f87011430c18e571
Johannes Kasser <johanneskasser@outlook.de>
2025-07-08 17:48:02 +0200

    Update pyqt5-tools version to 5.15.9.3.3 and ensure .mat file extension in save dialog

--- src/hdsemg_select/main.py
+++ src/hdsemg_select/main.py
@@ -1,6 +1,9 @@
 import argparse
 import logging
+import os
 import sys
+
+from PyQt5.QtCore import Qt
 
 from hdsemg_select._log.exception_hook import exception_hook
 from hdsemg_select._log.log_config import setup_logging
@@ -21,6 +24,10 @@ def main():
     parser.add_argument("--inputFile", type=str, help="File to be opened upon startup")
     parser.add_argument("--outputFile", type=str, help="Destination .mat file for saving the selection")
     args = parser.parse_args()
+
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)  # scale UI elements
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
     app = QApplication(sys.argv)
     window = ChannelSelector(input_file=args.inputFile, output_file=args.outputFile)
